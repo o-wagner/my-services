a607609e5eb0cf2c6dfe6fab3f4eba004f6acdb7
o-wagner <owagner.oliveira>
2023-03-21 13:43:22 -0300

    Page Categories

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,6 +1,11 @@
 import { useNavigation } from '@react-navigation/native';
 import { Text, StyleSheet, TouchableOpacity, Image, ImageBackground } from 'react-native';
 import Logo from '../../components/imgs/Vector.png';
+import Market from '../../components/imgs/Market.png';
+import Water from '../../components/imgs/Blur.png';
+import Ambulance from '../../components/imgs/Ambulance.png';
+import Box from '../../components/imgs/Box.png';
+import BackArrow from '../../components/imgs/BackArrow.png';
 
 export default function Home() {
     const navigation = useNavigation();
@@ -15,13 +20,28 @@ export default function Home() {
             resizeMode="stretch"
         >
             <Image source={Logo} resizeMode="contain" style={styles.logo} />
+            <Text style={styles.title}>CATEGORIAS</Text>
+
             <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
-                <Text style={styles.buttonText}>Voltar</Text>
+                <Image source={BackArrow} resizeMode="contain" style={styles.backArrow}/>
             </TouchableOpacity>
             
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Categories')}>
-                <Text style={styles.buttonText}>Categorias</Text>
+            <TouchableOpacity style={styles.button1} onPress={() => navigation.navigate('Categories')}>
+            <Image source={Market} resizeMode='stretch' style={styles.categoria1} />
             </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button2} onPress={() => navigation.navigate('Categories')}>
+            <Image source={Water} resizeMode='stretch' style={styles.categoria2} />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button3} onPress={() => navigation.navigate('Categories')}>
+            <Image source={Ambulance} resizeMode='stretch' style={styles.categoria3} />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button4} onPress={() => navigation.navigate('Categories')}>
+            <Image source={Box} resizeMode='stretch' style={styles.categoria4} />
+            </TouchableOpacity>
+
         </ImageBackground>
 
     )
@@ -33,27 +53,107 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
+
+    backArrow:{
+        position: 'absolute',
+        bottom: 232,
+        right: 100,
+        width: 30,
+    },
+
+    logo: {
+        width: 180,
+        position: 'absolute',
+        height: 100,
+        top: 90,
+    },
     title: {
-        color: 'orange',
-        fontSize: 50,
-        fontWeight: 'bold'
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+        position: 'relative',
+        bottom: 230
     },
     buttonText: {
-        backgroundColor: 'grey',
         color: 'white',
         fontSize: 16,
         fontWeight: 'bold',
         padding: 10,
-        borderRadius: 15,
-        marginTop: 10
-    },
-    logo: {
-        height: 50,
-        bottom: 200,
+        borderRadius: 18,
     },
 
-    categoria:{
-        
-    }
+    button1:{
+        backgroundColor: '#D9D9D9',
+        height: 120,
+        width: 120,
+        borderRadius: 8,
+        marginTop: 30,
+        position: 'absolute',
+        top: 280,
+        left: 60,
+        justifyContent: 'center',
+    },
+
+    button2:{
+        backgroundColor: '#d9d9d9',
+        height: 120,
+        width: 120,
+        borderRadius: 8,
+        marginTop: 30,
+        position: 'absolute',
+        top: 280,
+        left: 210,
+        justifyContent: 'center',
+    },
+    button3:{
+        backgroundColor: '#d9d9d9',
+        height: 120,
+        width: 120,
+        borderRadius: 8,
+        marginTop: 30,
+        position: 'absolute',
+        top: 430,
+        left: 60,
+        justifyContent: 'center',
+    },
+
+    button4:{
+        backgroundColor: '#d9d9d9',
+        height: 120,
+        width: 120,
+        borderRadius: 8,
+        marginTop: 30,
+        position: 'absolute',
+        top: 430,
+        left: 210,
+        justifyContent: 'center',
+    },
+
+    categoria1:{
+        position: 'absolute',
+        width: 55,
+        height: 55,
+        right: 34,
+    },
+    categoria2:{
+        position: 'absolute',
+        width: 55,
+        height: 55,
+        left: 34,
+    },
+    categoria3:{
+        position: 'absolute',
+        width: 55,
+        height: 55,
+        right: 34,
+    },
+
+    categoria4:{
+        position: 'absolute',
+        width: 60,
+        height: 60,
+        left: 30,
+    },
+    
 
 })
